0713369a38f3921011fd583dd34ebe5ddbc32c37
Mr.Jor <62343478+in-depth-1@users.noreply.github.com>
2023-05-29 09:22:26 +0800

    新增 alacritty 配置, picom 配置, rofi 配置, 相关启动脚本, 更新了 ideavim 配置

---
+++ scripts/callRofi.sh
@@ -0,0 +1,5 @@
+case "$1" in
+    run) rofi -show drun -show-icons ;;
+    # custom) rofi -show menu -modi 'menu:~/scripts/rofi.sh' -theme ~/scripts/config/rofi.rasi ;;
+    # window) rofi -show window -show-icons -theme ~/scripts/config/rofi_2c.rasi ;;
+esac
